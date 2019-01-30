6386fb7aa52d02212398093a7e4314bc51d83d5f
Jordan <jordanonuma@outlook.com>
2019-01-30 05:37:04 -1000

    Assigned variable 'dnaDigits' that will be stored to the blockchain.

--- Contract.sol
+++ Contract.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.4.25; //1. Enter solidity version here
 
 //2. Create contract here
 contract ZombieFactory {
-
+  uint dnaDigits = 16; //uint must be non-negative and are stored to the blockchain
 } //end contract ZombieFactory {}
