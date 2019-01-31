602477e61887b8160c613eb9847a28c827a350f5
Jordan <jordanonuma@outlook.com>
2019-01-31 05:41:29 -1000

    To keep numbers at 16 digits created a 10^16 divisor whose modulus will be 0 to 10^16-1 inclusive.

--- Contract.sol
+++ Contract.sol
@@ -3,4 +3,5 @@ pragma solidity ^0.4.25; //1. Enter solidity version here
 //2. Create contract here
 contract ZombieFactory {
   uint dnaDigits = 16; //uint must be non-negative and are stored to the blockchain
+  uint dnaModulus = 10 ** dnaDigits;
 } //end contract ZombieFactory {}
