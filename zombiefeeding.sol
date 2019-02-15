294129e7ad77a79e8ff753aae4d402da15e46dd8
Jordan <jordanonuma@outlook.com>
2019-02-15 05:57:52 -1000

    Added function feedAndMultiply() with logic check that user is the zombie owner. Then the zombie is stored on the blockchain in pointer 'myZombie'.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,5 +2,8 @@ pragma solidity ^0.4.25;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId]; //permament on blockchain as opposed to in memory
+  } //end function feedAndMultiply()
 } //end contract ZombieFeeding {}
