1f1be155bfdfba07fe677042e8c30348b14d2574
Jordan <jordanonuma@outlook.com>
2019-02-16 08:37:00 -1000

    Added logic to feedAndMultiply() that creates new zombie from new DNA that's averaged from the attacking zombie and the zombie being attacked.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,5 +5,9 @@ contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = zombies[_zombieId]; //permament on blockchain as opposed to in memory
+
+      _targetDna = _targetDna % dnaModulus; //takes last 16 digits of _targetDna
+      uint newDna = (myZombie.dna + _targetDna)/2;
+      _createZombie("NoName", newDna);
   } //end function feedAndMultiply()
 } //end contract ZombieFeeding {}
