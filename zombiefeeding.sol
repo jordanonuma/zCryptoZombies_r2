9e34dc157306465a577f6f248375a55a066696df
Jordan <jordanonuma@outlook.com>
2019-02-18 09:20:28 -1000

    Added interface to call cryptokitty's function getKitty().

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -1,6 +1,21 @@
 pragma solidity ^0.4.25;
 import "./zombiefactory.sol";
 
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+    );
+} //end contract KittyInterface {}
+
 contract ZombieFeeding is ZombieFactory {
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
