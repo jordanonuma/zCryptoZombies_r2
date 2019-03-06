92ed73b46aab571adc7e9735c8a2d54fe8287ce8
Jordan <jordanonuma@outlook.com>
2019-03-06 05:54:11 -1000

    Added function levelUp() that incrememnts defined zombie after checking if fee has been paid.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -10,6 +10,11 @@ contract ZombieHelper is ZombieFeeding {
     _;
   } //end modifier aboveLevel()
 
+  function levelUp(uint _zombieId) external payable {
+    require(msg.value == levelUpFee);
+    zombies[_zombieId].level++;
+  } //end function levelUp()
+
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
     require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
