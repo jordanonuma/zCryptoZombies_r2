4c0cc915e9c5d7bea05cda64c576c58995077f97
Jordan <jordanonuma@outlook.com>
2019-03-12 08:50:54 -1000

    Removed redundant require() code for the changeDna() and  changeName() functions.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -25,12 +25,10 @@ contract ZombieHelper is ZombieFeeding {
   } //end function levelUp()
 
   function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].name = _newName;
   } //end function changeName()
 
   function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
     zombies[_zombieId].dna = _newDna;
   } //end function changeDna()
 
