e89a022a8d5a888331ded79edbb1e6b93ebc4970
Jordan <jordanonuma@outlook.com>
2019-03-13 05:54:26 -1000

    Added ownerOf() modifier to function attack() to require user is the zombie's owner.

--- zombieattack.sol
+++ zombieattack.sol
@@ -10,7 +10,7 @@ function randMod(uint _modulus) internal returns(uint) {
   return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
 } //end function randMod()
 
-function attack(uint _zombieId, uint _targetId) external {
+function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
 
 } //end function attack()
 
