946979119268305136d1bdd5cfa00f3cfac6c1c3
Jordan <jordanonuma@outlook.com>
2019-03-19 09:14:55 -1000

    Added frames of ERC721 functions that will be used in zombieownership.sol.

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,5 +3,19 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+  function balanceOf(address _owner) external view returns (uint256) {
 
+  } //end function balanceOf()
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+
+  } //end function ownerOf()
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+
+  } //end function transferFrom()
+
+  function approve(address _approved, uint256 _tokenId) external payable {
+
+  } //end function approve()
 } //end contract ZombieOwnership {}
