0c97876292cea1d68d44f992773d170a4f8f71b7
Jordan <jordanonuma@outlook.com>
2019-03-25 05:51:16 -1000

    Added import and declaration using SafeMath for uint256.

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.25; //1. Enter solidity version here
 import "./ownable.sol";
+import "./safemath.sol";
 
 //2. Create contract here
 contract ZombieFactory is Ownable {
+  using SafeMath for uint256;
   event NewZombie(uint zombieId, string name, uint dna);
 
   uint dnaDigits = 16; //uint must be non-negative and are stored to the blockchain
