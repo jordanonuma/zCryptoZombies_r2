9581d67222d2e635332eeda8e07fceff4d4add23
Jordan <jordanonuma@outlook.com>
2019-04-08 05:19:02 -1000

    Declared and initialized gameState struct to read in all previous states of the game.

--- zb_game_mode-master/contracts/Core/ZB/examplegame.sol
+++ zb_game_mode-master/contracts/Core/ZB/examplegame.sol
@@ -3,8 +3,9 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
-  function beforeMatchStart(bytes serializedGameState) external{
-
+  function beforeMatchStart(bytes serializedGameState) external {
+    GameState memory gameState;
+    gameState.init(serializedGameState);
   } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
