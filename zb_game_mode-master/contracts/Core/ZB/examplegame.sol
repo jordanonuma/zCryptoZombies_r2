910c494811ba8709ddbe24d74ab28f4eba7c64fd
Jordan <jordanonuma@outlook.com>
2019-04-09 05:58:25 -1000

    Declared and initialized a custom data type that tracks any changes to the game's state.

--- zb_game_mode-master/contracts/Core/ZB/examplegame.sol
+++ zb_game_mode-master/contracts/Core/ZB/examplegame.sol
@@ -6,6 +6,9 @@ contract ExampleGame is ZBGameMode  {
   function beforeMatchStart(bytes serializedGameState) external {
     GameState memory gameState;
     gameState.init(serializedGameState);
+
+    ZBSerializer.SerializedGameStateChanges memory changes; //declares custom data type that tracks all changes made to game state
+    changes.init(); 
   } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
