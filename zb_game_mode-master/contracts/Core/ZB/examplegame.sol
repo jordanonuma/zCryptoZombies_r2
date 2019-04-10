5f62a3e7e471571ee7850c9707b4436d130d4e7a
Jordan <jordanonuma@outlook.com>
2019-04-10 05:30:36 -1000

    Added initial states (using 'changes') to enum 'Player' for both players 1 and 2.

--- zb_game_mode-master/contracts/Core/ZB/examplegame.sol
+++ zb_game_mode-master/contracts/Core/ZB/examplegame.sol
@@ -8,7 +8,17 @@ contract ExampleGame is ZBGameMode  {
     gameState.init(serializedGameState);
 
     ZBSerializer.SerializedGameStateChanges memory changes; //declares custom data type that tracks all changes made to game state
-    changes.init(); 
+    changes.init();
+
+    //Sets the defense and initial available, filled, and max vials for both players
+    changes.changePlayerDefense(Player.Player1, 15);
+    changes.changePlayerDefense(Player.Player2, 15);
+    changes.changePlayerCurrentGooVials(Player.Player1, 3);
+    changes.changePlayerCurrentGooVials(Player.Player2, 3);
+    changes.changePlayerCurrentGoo(Player.Player1, 3);
+    changes.changePlayerCurrentGoo(Player.Player2, 3);
+    changes.changePlayerMaxGooVials(Player.Player1, 8);
+    changes.changePlayerMaxGooVials(Player.Player2, 8);
   } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
