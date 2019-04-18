fa82706e19f8bcb42f4e6f685e3e4f9db6a9b4af
Jordan <jordanonuma@outlook.com>
2019-04-18 06:01:54 -1000

    Added function returns token symbol.

--- ERC721x/ZombieToken.sol
+++ ERC721x/ZombieToken.sol
@@ -6,4 +6,8 @@ contract ZombieCard is ERC721XToken {
   function name() external view returns (string) {
       return "ZombieCard";
   } //end function name()
+
+  function symbol() external view returns (string) {
+    return "ZCX";
+  } //end function symbol()
 } //end contract {}
